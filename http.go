109f8672ae51445a47a782c6120c5bdc1c85ccbe
Fan Zhou <>
2016-11-23 18:54:50 +0800

    Is if in China?

--- http.go
+++ http.go
@@ -1,13 +1,28 @@
 package ext
 
 import (
+	"encoding/json"
+	"io"
 	"net/http"
 	"net/http/httptest"
 )
 
-func PerformHttpRequest(r http.Handler, method, path string) *httptest.ResponseRecorder {
+func PerformHttpRequest(r http.Handler, method, path string, header http.Header) *httptest.ResponseRecorder {
 	req, _ := http.NewRequest(method, path, nil)
+	if header != nil {
+		req.Header = header
+	}
 	w := httptest.NewRecorder()
 	r.ServeHTTP(w, req)
 	return w
+}
+
+func DecodeJson(r io.Reader) map[string]interface{} {
+	var m map[string]interface{}
+	decoder := json.NewDecoder(r)
+	err := decoder.Decode(&m)
+	if err == nil {
+		return m
+	}
+	return nil
 }
